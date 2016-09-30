e3cfda6547c14957cc1fc58b7a8062a76af1fd32
Akshar <mistry.aks@gmail.com>
2016-10-01 02:18:20 +0530

    Added CRUD to restraunt

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ mongoose.connect("mongodb://localhost/FST");  	//incomplete statement, add confi
 //initialize express server
 var app = express();
 app.use(bodyparser.urlencoded({extended:true}));
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 app.use(logger('dev'));
 
+
+app.get('/:tag',function(req,res){
+
+    console.log("the vioudsbls: "+req.params.tag);
+
+});
 
 //routes
 app.use("/admin/rest",require('./routes/restaurant'))
@@ -21,7 +27,7 @@ app.use("/admin/login",require('./routes/user'))
 
 app.use("/admin/test",require('./routes/testAPI'))
 
-
+app.use("/admin/updaterest",require('./routes/testing'))
 
 app.use(express.static(__dirname+'/assets'));
 
